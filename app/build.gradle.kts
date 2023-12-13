d8a1c5d701d00e8404365894dcb99f088f4f4685
Hamzah-Manzoor <hamza88851@gmail.com>
2023-12-13 15:34:05 +0500

    Made Changes In build.gradle.kts file.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,7 @@ dependencies {
     implementation("com.google.android.engage:engage-core:1.3.1")
     implementation("androidx.compose.foundation:foundation-android:1.5.4")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
+    implementation("com.google.firebase:firebase-firestore-ktx:24.10.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
